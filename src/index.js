b4d369cf91ffb6ae1a439115c2602a12308bf729
Katya Chuprina <katyachuprina@MacBook-Air-Katya.local>
2023-01-17 21:09:40 +0300

    docs: add finite solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort (matrix=[]) {
   let array = [];
   for (let i=0; i < matrix.length; i++) {
     for (let j=0; j < matrix[i].length; j++) {
